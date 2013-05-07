2a5fd112ca3a7fd51aec4285b23316d3da1d32d5
Philipp Küng <git@philippkueng.ch>
2013-05-07 19:55:01 +0200

    after fetching the index, go ahead and fetch the names for each year -> still buggy and only pages one way

--- public/scrape_firstname_index.js
+++ public/scrape_firstname_index.js
@@ -1,30 +1,47 @@
 (function(jQuery){
   jQuery(function(){
 
+    var searchYear = null;
+
     var processPage = function(){
       jQuery('#search_result tr td a.name_link').each(function(key, value){
         jQuery.getJSON('http://localhost:3000/firstnames/create?callback=?', {
           name: jQuery(value).text(),
           rank: jQuery(value).parent().parent().find('td:first').text(),
-          count: jQuery(value).parent().parent().find('td.total_right>div').text()
+          count: jQuery(value).parent().parent().find('td.total_right>div').text(),
+          sex: 'male'
         }, function(data){
-          console.log('saved #' + jQuery(value).parent().parent().find('td:first').text() + ' - ' + jQuery(value).text());
+          console.log('saved #' + jQuery(value).parent().parent().find('td:first').text() + ' - ' + jQuery(value).text() + ' - Year: ' + searchYear);
         });
       });
 
       setTimeout(function(){
-        if(jQuery('#search_result table.search tr:last td a.pages')){
-          var data = jQuery('#search_result table.search tr:last td a.pages').attr("attr-data");
-          do_search_request(data);
+        if(jQuery('#search_result table.search tr:last td').length !== 0){
+            var data = jQuery('#search_result table.search tr:last td a.pages').attr("attr-data");
+            do_search_request(data);
+        } else {
+          if(searchYear && searchYear != 2011){
+            searchYear = searchYear + 1;
+          } else if(!searchYear) {
+            searchYear = 1902;
+          }
+          
+          // set the search_to_year and search_to_from selects
+          jQuery('#search_to_year option[value="' + searchYear + '"]').attr('selected', 'selected');
+          jQuery('#search_from_year option[value="' + searchYear + '"]').attr('selected', 'selected');
+          jQuery('#search_button').click();
+          setTimeout(function(){
+            processPage();
+          }, 10000);
         }
       }, 3000);
     };
 
     this.do_search_request = function(data) {
-        // if(AutoComplete) {
-        //     AutoComplete.hide();
-        // }
-        // Search = true;
+        if(AutoComplete) {
+            AutoComplete.hide();
+        }
+        Search = true;
         jQuery.ajax({
             url: 'index.php?route=statistic/search/search',
             type: 'post',
@@ -64,7 +81,7 @@
                     var year_str = "&search_from_year=" + jQuery("#search_form input[name='search_from_year']").val();
                     year_str += "&search_to_year=" + jQuery("#search_form input[name='search_to_year']").val();
                     jQuery('#search_result').html(json['html']);
-                    // Search = false;
+                    Search = false;
 
                     // execute some scraper code
                     processPage();
@@ -73,6 +90,73 @@
         }); 
     };
     window.do_search_request = this.do_search_request;
+    
+    this.search = function(){
+        if(AutoComplete) {
+            AutoComplete.hide();
+        }
+        Search = true;
+        if(!jQuery("#search_from_year").val() && !jQuery("#search_to_year").val()) {
+            jQuery("#search_from_year").val( jQuery("#default_search_from_year").val() );
+            jQuery("#search_to_year").val( jQuery("#default_search_to_year").val() );
+        }
+        var sex_str = '';
+        if(jQuery("#search_form input[name='sex']:checked").val() != undefined) {
+            sex_str = "&sex=" + jQuery("#search_form input[name='sex']:checked").val();
+        }
+        jQuery.ajax({
+          url: 'index.php?route=statistic/search/search',
+          type: 'post',
+          data: jQuery('#search_form input[type=\'text\'], #search_form input[type=\'hidden\'], #search_form input[type=\'radio\']:checked, #search_form select'),
+          dataType: 'json',
+          cache: false,
+          async: true,
+          beforeSend: function() {
+            jQuery('#sex_choose').css('display','none');
+            jQuery('#sex_choose input[type=\'radio\']').attr('checked',false);
+            jQuery('#search_result').empty();
+            jQuery('#search_button').attr('disabled', true);
+            jQuery('#reset_button').attr('disabled', true);
+            if(jQuery('#reset_button').length > 0) {
+                jQuery('#reset_button').after('<span class="wait">&nbsp;<img src="catalog/view/theme/default/image/loading.gif" alt="" /></span>');
+            } else {
+                jQuery('#search_button').after('<span class="wait">&nbsp;<img src="catalog/view/theme/default/image/loading.gif" alt="" /></span>');
+            }
+          },  
+          complete: function() {
+            jQuery('#search_button').attr('disabled', false);
+            jQuery('#reset_button').attr('disabled', false);
+            jQuery('.wait').remove();
+          },      
+          success: function(json) {
+            jQuery('.error').remove();
+            if (json['error']) {
+              var error = '';
+              for(e in json['error']) {
+                  error += json['error'][e] + '<br />';
+              }
+              jQuery('#search_form').prepend('<div class="error" style="display: none;">' + error + '</div>');
+              jQuery('.error').fadeIn('slow');
+              Search = false;
+            } else if(json['sex_choose']) {
+              jQuery('#sex_choose').fadeIn();
+            } else {
+              var year_str = "&search_from_year=" + jQuery("#search_form input[name='search_from_year']").val();
+              year_str += "&search_to_year=" + jQuery("#search_form input[name='search_to_year']").val();
+              if(jQuery('#main_search').length > 0) {
+                  location = 'index.php?route=statistic/search&q=' + jQuery("#search_form input[type='text']").val() + sex_str + year_str;
+              } else {
+                  jQuery('#search_result').html(json['html']);
+              }
+              Search = false;
+              
+              // execute the scrape for the specific year
+              processPage();
+            }
+          }
+        });
+    }
+    window.search = this.search;
 
     processPage();
   });
