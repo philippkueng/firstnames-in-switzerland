8f9f4e7993d540301a098cec7fc9ba16b7fd56c2
Philipp Küng <git@philippkueng.ch>
2013-05-09 12:54:02 +0200

    enabling plpgsql extension

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130505164514) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "firstnames", force: true do |t|
     t.string   "name"
     t.integer  "rank"
