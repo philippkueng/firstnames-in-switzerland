0ef37d793efd22043fdc11fe757a07bd26b9c64f
Philipp Küng <git@philippkueng.ch>
2013-08-06 22:31:25 +0200

    adding a sleep between the fetches reduce the load on google and hopefully not trigger the limit as fast; rewriting csv files after the download

--- google-trends-scraper/fetch_trends.py
+++ google-trends-scraper/fetch_trends.py
@@ -6,11 +6,12 @@ import time
 GOOGLE_TRENDS_URL = "http://www.google.com/trends/trendsReport?hl=en-US&cmpt=q&content=1&export=1&"
 
 # open the file and read it line by line
-names = open("distinct-names-sample.txt", "r").read().split('\n')
+names = open("distinct-names.txt", "r").read().split('\n')
 
 # fetch the trends file for each name
 http = urllib3.PoolManager()
 for name in names:
 	print urlencode({'q': name.lower()})
 	os.system("/usr/bin/open -a Google\ Chrome '" + GOOGLE_TRENDS_URL + urlencode({'q': name.lower()}) + "'")
-	time.sleep(4)
+	time.sleep(30)
+	os.rename('data/report.csv', 'data/' + name.lower() + '.csv')
